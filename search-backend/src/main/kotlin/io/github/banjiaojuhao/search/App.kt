2324f71a13479ac5ed64af5518c342e41cd0cab0
banjiaojuhao <banjiaojuhao@qq.com>
2020-07-25 21:40:09 +0800

    finish project

--- search-backend/src/main/kotlin/io/github/banjiaojuhao/search/App.kt
+++ search-backend/src/main/kotlin/io/github/banjiaojuhao/search/App.kt
@@ -1,41 +1,31 @@
 package io.github.banjiaojuhao.search
 
 import io.github.banjiaojuhao.search.db.StoreConnection
-import io.github.banjiaojuhao.search.db.WebPageTable
 import io.vertx.core.Vertx
-import io.vertx.core.json.JsonArray
-import io.vertx.core.json.JsonObject
 import io.vertx.ext.web.Router
 import io.vertx.ext.web.handler.StaticHandler
 import io.vertx.kotlin.core.http.httpServerOptionsOf
 import io.vertx.kotlin.core.http.listenAwait
-import io.vertx.kotlin.core.json.JsonArray
-import io.vertx.kotlin.core.json.jsonObjectOf
 import io.vertx.kotlin.coroutines.awaitEvent
 import kotlinx.coroutines.runBlocking
 import org.apache.lucene.analysis.cn.smart.SmartChineseAnalyzer
-import org.apache.lucene.document.*
 import org.apache.lucene.index.DirectoryReader
-import org.apache.lucene.index.IndexWriter
-import org.apache.lucene.index.IndexWriterConfig
 import org.apache.lucene.queryparser.classic.MultiFieldQueryParser
 import org.apache.lucene.search.IndexSearcher
 import org.apache.lucene.store.FSDirectory
-import org.jetbrains.exposed.sql.select
-import org.jsoup.Jsoup
 import sun.misc.Signal
 import java.nio.file.Paths
-import java.time.LocalDateTime
-import java.time.ZoneOffset
-import java.time.format.DateTimeFormatter
 
 
-private val dbConnection = StoreConnection()
+data class SearchResult(val title: String, val articleId: String)
+
+internal val dbConnection = StoreConnection()
 
 fun main(args: Array<String>) = runBlocking<Unit> {
-    println("make index?[y/n]")
-    if (readLine() == "y") {
+    if (args.isNotEmpty() && args[0] == "index") {
+        println("making index")
         makeIndex()
+        println("finish making index")
     }
 
     val vertx = Vertx.vertx()
@@ -49,25 +39,47 @@ fun main(args: Array<String>) = runBlocking<Unit> {
 
     router.route().handler(StaticHandler.create())
 
-    val errorEmptyQuery = jsonObjectOf(
-        "code" to 1,
-        "msg" to "non query string"
-    ).toString()
-    router.get("/query").handler { routingContext ->
-        val query = routingContext.queryParams()["q"]
-        if (query == null || query == "") {
-            routingContext.response().end(errorEmptyQuery)
+    val template = this::class.java.getResourceAsStream("/webroot/google-result.html").use {
+        it.readAllBytes().toString(Charsets.UTF_8)
+    }
+
+    router.get("/search").handler { routingContext ->
+        val keyword = routingContext.queryParams()["q"]
+        if (keyword == null || keyword == "") {
+            routingContext.response()
+                .setStatusCode(503)
+                .end("invalid query string")
         } else {
-            vertx.executeBlocking<JsonObject>({
-                it.complete(query(query))
+            vertx.executeBlocking<String>({
+                val resultHtml = query(keyword)
+                    .joinToString(separator = "") {
+                        """<div class="g">
+                        <div class="rc">
+                            <div class="r"><a href="http://www.hitsz.edu.cn/article/view/id-${it.articleId}.html"><br>
+                                <h3 class="LC20lb DKV0Md">${it.title}</h3>
+                            </a>
+                            </div>
+                            <div class="s">
+                                <div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>"""
+                    }
+
+                it.complete(resultHtml)
             }) {
-                routingContext.response().end(it.result().toBuffer())
+                routingContext.response()
+                    .end(template
+                        .replace("<!--keyword-->", keyword)
+                        .replace("<!--result-->", it.result()))
             }
         }
 
     }
 
     httpServer.listenAwait(8080)
+    println("server started, visit: http://localhost:8080/")
 
     awaitEvent<Unit> { handler ->
         Signal.handle(Signal("INT")) {
@@ -80,61 +92,17 @@ fun main(args: Array<String>) = runBlocking<Unit> {
 }
 
 
-private val formatterYMDHM = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
-private suspend fun makeIndex() {
-    val indexDir = FSDirectory.open(Paths.get("index"))
-    val analyzer = SmartChineseAnalyzer()
-    val indexWriterConfig = IndexWriterConfig(analyzer)
-    indexWriterConfig.openMode = IndexWriterConfig.OpenMode.CREATE
-    IndexWriter(indexDir, indexWriterConfig).use { indexWriter ->
-        dbConnection.execute {
-            WebPageTable.slice(WebPageTable.articleId, WebPageTable.webPage).select {
-                WebPageTable.state eq 2
-            }.toList()
-        }.forEach {
-            val doc = Document()
-            val articleId = it[WebPageTable.articleId]
-
-            val dom = Jsoup.parse(it[WebPageTable.webPage])
-            val title = dom.select("div.title").text()
-            val tip = dom.select("div.tip > span.item")
-            if (tip.size != 2) {
-                println("error in article $articleId")
-                return@forEach
-            }
-            val issueTimeText = tip[0].text().substringAfter("：").trim()
-            val issueTime = LocalDateTime.parse(issueTimeText, formatterYMDHM)
-                .toEpochSecond(ZoneOffset.ofHours(8))
-            val viewed = tip[1].text().trim().toInt()
-            val content = dom.select("div.detail").text()
-
-            doc.add(StoredField("article_id", articleId))
-            doc.add(TextField("title", title.reader()))
-            doc.add(TextField("content", content.reader()))
-            doc.add(LongPoint("issue_time", issueTime))
-            doc.add(IntPoint("view", viewed))
-            indexWriter.addDocument(doc)
-            println("finish index article $articleId")
-        }
-    }
-}
-
-private fun query(queryText: String): JsonObject {
+private fun query(queryText: String): List<SearchResult> {
     val fsDirectory = FSDirectory.open(Paths.get("index"))
     val reader = DirectoryReader.open(fsDirectory)
     val indexSearcher = IndexSearcher(reader)
     val parser = MultiFieldQueryParser(arrayOf("title", "content"), SmartChineseAnalyzer())
     val query = parser.parse(queryText)
     val topDocs = indexSearcher.search(query, 100)
-    val resultList = topDocs.scoreDocs.map {
+    return topDocs.scoreDocs.map {
         val document = indexSearcher.doc(it.doc)
         val title = document["title"]
         val articleId = document["article_id"]
-        jsonObjectOf("title" to title, "article_id" to articleId)
+        SearchResult(title, articleId)
     }
-    return jsonObjectOf(
-        "code" to 0,
-        "msg" to "ok",
-        "data" to JsonArray(resultList)
-    )
 }
---
+++ search-backend/src/main/kotlin/io/github/banjiaojuhao/search/MakeIndex.kt
@@ -0,0 +1,53 @@
+package io.github.banjiaojuhao.search
+
+import io.github.banjiaojuhao.search.db.WebPageTable
+import org.apache.lucene.analysis.cn.smart.SmartChineseAnalyzer
+import org.apache.lucene.document.*
+import org.apache.lucene.index.IndexWriter
+import org.apache.lucene.index.IndexWriterConfig
+import org.apache.lucene.store.FSDirectory
+import org.jetbrains.exposed.sql.select
+import org.jsoup.Jsoup
+import java.nio.file.Paths
+import java.time.LocalDateTime
+import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
+
+private val formatterYMDHM = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+internal suspend fun makeIndex() {
+    val indexDir = FSDirectory.open(Paths.get("index"))
+    val analyzer = SmartChineseAnalyzer()
+    val indexWriterConfig = IndexWriterConfig(analyzer)
+    indexWriterConfig.openMode = IndexWriterConfig.OpenMode.CREATE
+    IndexWriter(indexDir, indexWriterConfig).use { indexWriter ->
+        dbConnection.execute {
+            WebPageTable.slice(WebPageTable.articleId, WebPageTable.webPage).select {
+                WebPageTable.state eq 2
+            }.toList()
+        }.forEach {
+            val doc = Document()
+            val articleId = it[WebPageTable.articleId]
+
+            val dom = Jsoup.parse(it[WebPageTable.webPage])
+            val title = dom.select("div.title").text()
+            val tip = dom.select("div.tip > span.item")
+            if (tip.size != 2) {
+                println("error in article $articleId")
+                return@forEach
+            }
+            val issueTimeText = tip[0].text().substringAfter("：").trim()
+            val issueTime = LocalDateTime.parse(issueTimeText, formatterYMDHM)
+                .toEpochSecond(ZoneOffset.ofHours(8))
+            val viewed = tip[1].text().trim().toInt()
+            val content = dom.select("div.detail").text()
+
+            doc.add(StoredField("article_id", articleId))
+            doc.add(TextField("title", title, Field.Store.YES))
+            doc.add(TextField("content", content.reader()))
+            doc.add(LongPoint("issue_time", issueTime))
+            doc.add(IntPoint("view", viewed))
+            indexWriter.addDocument(doc)
+            println("finish index article $articleId")
+        }
+    }
+}
