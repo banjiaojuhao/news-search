b878bd4ae10e961ede1d7f61baf482555d9e5c56
banjiaojuhao <banjiaojuhao@qq.com>
2020-07-25 22:05:21 +0800

    add export url

--- search-backend/src/main/kotlin/io/github/banjiaojuhao/search/App.kt
+++ search-backend/src/main/kotlin/io/github/banjiaojuhao/search/App.kt
@@ -1,11 +1,19 @@
 package io.github.banjiaojuhao.search
 
 import io.github.banjiaojuhao.search.db.StoreConnection
+import io.github.banjiaojuhao.search.db.WebPageTable
 import kotlinx.coroutines.runBlocking
+import org.jetbrains.exposed.sql.selectAll
+import java.nio.file.Files
+import java.nio.file.Path
 
 
 fun main(args: Array<String>) = runBlocking<Unit> {
-    if (args.isNotEmpty() && args[0] == "index") {
+    if (args.contains("export")) {
+        exportUrl()
+        println("exported url into url.txt")
+    }
+    if (args.contains("index")) {
         println("making index")
         StoreConnection().use {
             makeIndex(it)
@@ -13,5 +21,16 @@ fun main(args: Array<String>) = runBlocking<Unit> {
         println("finish making index")
     }
     runSearchServer()
+}
+
+private suspend fun exportUrl() {
+    StoreConnection().use {
+        val urlList = it.execute {
+            WebPageTable.slice(WebPageTable.articleId).selectAll().map { it[WebPageTable.articleId] }
+        }.joinToString(separator = "\n") { "http://www.hitsz.edu.cn/article/view/id-$it.html" }
+        Files.newBufferedWriter(Path.of("url.txt")).use {
+            it.write(urlList)
+        }
+    }
 }
 
